d3451f389be5538e6afc7dc30d572ebcaa87c602
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-22 16:19:58 -0700

    Blocked out Nav compnonent html and links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav(props) {
-    return (
-        <div>
-            hello world!
-        </div>
-    );
+	return (
+		<div>
+			<nav>
+				<ul>
+					<Link to='/books'>All Books</Link>
+					<Link to='/books/:genre'>Fantasy</Link>
+					<Link to='/books/:genre'>Mystery</Link>
+					<Link to='/books/:genre'>Romance</Link>
+					<Link to='/books/:genre'>Fiction</Link>
+					<Link to='/books/:genre'>Non-Fiction</Link>
+				</ul>
+				<Link to='/add-book'>
+					<button>Submit Your Favorite Book</button>
+				</Link>
+			</nav>
+		</div>
+	);
 }
 
 export default Nav;
