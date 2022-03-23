392bad5b50111d340f8bc699f12ef47f79a07947
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-23 15:35:27 -0700

    Restructured component routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Navigate } from 'react-router-dom';
 import Book from './components/Book/Book';
 import BookList from './components/BookList/BookList';
 import Form from './components/Form/Form';
 import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+	// const navigate = useNavigate();
+
+	// function handleClick() {
+	// 	navigate('/');
+	// }
+	const iconBook = <FontAwesomeIcon icon={faBook} />;
+
 	return (
 		<>
 			<header id='page-top'>
 				<div id='button-nav-container'>
-					<button id='button-nav'>Nav Icon</button>
+					<button id='button-nav'>
+						<FontAwesomeIcon icon={faBook} className='icon'></FontAwesomeIcon>
+					</button>
 				</div>
+
 				<div id='title-nav-container'>
 					<div id='title-nav'>
 						<Link to='/'>
@@ -20,27 +33,16 @@ function App() {
 					</div>
 				</div>
 			</header>
-			<main id='main-content'>
-				<Nav />
-				<div id='roulette-container'>
-					<div>
-						<h2>Find Your Next Favorite Book</h2>
-					</div>
-					<input type='text' />
-					<img
-						src='https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
-						alt=''
-						id='roulette-img'
-					/>
-					<button id='roulette-button'>Get Suggestion</button>
-				</div>
+			<Nav />
+			<div>
 				<Routes>
 					<Route path='/books' element={<BookList />} />
 					<Route path='/books/:genre' element={<BookList />} />
 					<Route path='/add-book' element={<Form />} />
 					<Route path='/:genre/:id' element={<Book />} />
+					<Route path='/' element={<Home />} />
 				</Routes>
-			</main>
+			</div>
 		</>
 	);
 }
