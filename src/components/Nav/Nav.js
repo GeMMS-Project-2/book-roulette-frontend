ff47af86e8585c45a23f2f55bef4708bc1226643
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-23 19:56:34 -0700

    Added active class for CSS purposes to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,33 +3,31 @@ import { Link } from 'react-router-dom';
 
 function Nav(props) {
 	return (
-		<div id='nav-container'>
-			<nav>
-				<ul>
-					<Link to='/books'>
-						<li className='nav-link'>All Books</li>
-					</Link>
-					<Link to='/books/:genre'>
-						<li className='nav-link'>Fantasy</li>
-					</Link>
-					<Link to='/books/:genre'>
-						<li className='nav-link'>Mystery</li>
-					</Link>
-					<Link to='/books/:genre'>
-						<li className='nav-link'>Romance</li>
-					</Link>
-					<Link to='/books/:genre'>
-						<li className='nav-link'>Fiction</li>
-					</Link>
-					<Link to='/books/:genre'>
-						<li className='nav-link'>Non-Fiction</li>
-					</Link>
-				</ul>
-				<Link to='/add-book'>
-					<button id='add-book'>Submit Your Favorite Book</button>
+		<nav className='nav active' id='nav-genres'>
+			<ul className='nav-ul'>
+				<Link to='/books'>
+					<li className='nav-link'>All Books</li>
 				</Link>
-			</nav>
-		</div>
+				<Link to='/books/:genre'>
+					<li className='nav-link'>Fantasy</li>
+				</Link>
+				<Link to='/books/:genre'>
+					<li className='nav-link'>Mystery</li>
+				</Link>
+				<Link to='/books/:genre'>
+					<li className='nav-link'>Romance</li>
+				</Link>
+				<Link to='/books/:genre'>
+					<li className='nav-link'>Fiction</li>
+				</Link>
+				<Link to='/books/:genre'>
+					<li className='nav-link'>Non-Fiction</li>
+				</Link>
+			</ul>
+			<Link to='/add-book'>
+				<button id='add-book'>Submit Your Favorite Book</button>
+			</Link>
+		</nav>
 	);
 }
 
