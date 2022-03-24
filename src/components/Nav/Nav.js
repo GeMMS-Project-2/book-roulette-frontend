e0cac782484429801fb3c601a961ea8f12833ae5
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-24 10:25:33 -0700

    Added state variables to set class active or inactive on the nav element

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function Nav(props) {
+function Nav({ classState }) {
+	useEffect(() => {
+		console.log('hello');
+	}, [classState]);
+
 	return (
-		<nav className='nav active' id='nav-genres'>
-			<ul className='nav-ul'>
-				<Link to='/books'>
-					<li className='nav-link'>All Books</li>
+		<>
+			<nav className={`${classState}` + ' nav'} id='nav-genres'>
+				<ul className='nav-ul'>
+					<Link to='/books'>
+						<li className='nav-link'>All Books</li>
+					</Link>
+					<Link to='/books/:genre'>
+						<li className='nav-link'>Fantasy</li>
+					</Link>
+					<Link to='/books/:genre'>
+						<li className='nav-link'>Mystery</li>
+					</Link>
+					<Link to='/books/:genre'>
+						<li className='nav-link'>Romance</li>
+					</Link>
+					<Link to='/books/:genre'>
+						<li className='nav-link'>Fiction</li>
+					</Link>
+					<Link to='/books/:genre'>
+						<li className='nav-link'>Non-Fiction</li>
+					</Link>
+				</ul>
+				<Link to='/add-book'>
+					<button id='add-book'>Submit Your Favorite Book</button>
 				</Link>
-				<Link to='/books/:genre'>
-					<li className='nav-link'>Fantasy</li>
-				</Link>
-				<Link to='/books/:genre'>
-					<li className='nav-link'>Mystery</li>
-				</Link>
-				<Link to='/books/:genre'>
-					<li className='nav-link'>Romance</li>
-				</Link>
-				<Link to='/books/:genre'>
-					<li className='nav-link'>Fiction</li>
-				</Link>
-				<Link to='/books/:genre'>
-					<li className='nav-link'>Non-Fiction</li>
-				</Link>
-			</ul>
-			<Link to='/add-book'>
-				<button id='add-book'>Submit Your Favorite Book</button>
-			</Link>
-		</nav>
+			</nav>
+		</>
 	);
 }
 
