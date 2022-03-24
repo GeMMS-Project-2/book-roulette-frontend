a9e406281cb89dc1d5d0924097598165e8586caf
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-23 19:55:23 -0700

    Made changes to roulette input field

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,4 @@
+import './Home.css';
 export default function Home() {
 	return (
 		<main id='main-content'>
@@ -5,7 +6,20 @@ export default function Home() {
 				<div>
 					<h2>Find Your Next Favorite Book</h2>
 				</div>
-				<input type='text' />
+				<label htmlFor='genre'>Genre: </label>
+				<input
+					list='genres'
+					id='genre-roulette-input'
+					placeholder='Genre'
+					required
+				/>
+				<datalist id='genre-roulette-list'>
+					<option value='Fantasy'></option>
+					<option value='Mystery'></option>
+					<option value='Thriller'></option>
+					<option value='Education'></option>
+					<option value='Romance'></option>
+				</datalist>
 				<img
 					src='https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
 					alt=''
