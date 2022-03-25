4b2fd0e914182c171cf00eac62131ce845575e76
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-24 20:50:29 -0700

    mb added randomly generated book recommendation functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,33 +1,41 @@
 import './Home.css';
 import { useState } from 'react';
+import Book from '../Book/Book';
+import { Link } from 'react-router-dom';
 
-export default function Home() {
+export default function Home({ genre }) {
+	const [searchGenre, setSearchGenre] = useState('');
+	function handleSelect(e) {
+		setSearchGenre(e.target.value);
+		console.log(searchGenre);
+	}
+
 	return (
 		<main id='main-content'>
 			<div id='roulette-container'>
 				<div>
 					<h2>Find Your Next Favorite Book</h2>
 				</div>
-				<label htmlFor='genre'>Genre: </label>
-				<input
-					list='genres'
-					id='genre-roulette-input'
-					placeholder='Genre'
-					required
-				/>
-				<datalist id='genre-roulette-list'>
-					<option value='Fantasy'></option>
-					<option value='Mystery'></option>
-					<option value='Thriller'></option>
-					<option value='Education'></option>
-					<option value='Romance'></option>
-				</datalist>
+				<form action='' onChange={handleSelect}>
+					<label htmlFor='selectGenre'>Pick a genre:</label>
+					<select name='selectGenre' id='selectGenre' className='drop-down'>
+						<option value=''>Select..</option>
+						<option value='Fantasy'>Fantasy</option>
+						<option value='Romance'>Romance</option>
+						<option value='Educational'>Educational</option>
+						<option value='Mystery'>Mystery</option>
+						<option value='Thriller'>Thriller</option>
+					</select>
+				</form>
 				<img
 					src='https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
 					alt=''
 					id='roulette-img'
 				/>
-				<button id='roulette-button'>Get Suggestion</button>
+
+				<Link to={`/${searchGenre}`}>
+					<button id='roulette-button'>Find your next book</button>
+				</Link>
 			</div>
 		</main>
 	);
