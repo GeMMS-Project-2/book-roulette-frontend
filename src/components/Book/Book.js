7970be5f95ee5d85f4a0b55ee09a73e8409f650c
michellechoi126 <michellechoi126@gmail.com>
2022-03-24 23:00:30 -0700

    updated fetch endpoint

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -4,9 +4,8 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 import "./Book.css";
 
-
 function Book() {
-    const { id } = useParams();
+    const { id, genre } = useParams();
     const navigate = useNavigate();
     const [book, setBook] = useState(null);
     const [modal, setModal] = useState(false);
@@ -15,13 +14,13 @@ function Book() {
     const [active, setActive] = useState("")
 
     useEffect(() => {
-        fetch(`https://book-roulette.herokuapp.com/books/${id}`)
+        fetch(`https://book-roulette.herokuapp.com/books/genre/${genre}/random`)
             .then(res => res.json())
             .then(data => {
                 setBook(data);
             })
             .catch(err => console.log(err));
-    }, [id]);
+    }, []);
 
     if (!book) {
         return <h1>Loading...</h1>;
@@ -51,7 +50,7 @@ function Book() {
     const handleSubmit = async (event) => {
 		event.preventDefault();
 		try {
-			const res = await axios.put(`https://book-roulette.herokuapp.com/books/${id}`, book);
+			const res = await axios.put(`https://book-roulette.herokuapp.com/books/${book._id}`, book);
 			if (res.status === 200) {
 				setModal(false);
 			}
@@ -65,7 +64,7 @@ function Book() {
 		const confirm = window.confirm("Are you sure you want to delete?");
 		if (confirm) {
 			try {
-				const res = await axios.delete(`https://book-roulette.herokuapp.com/books/${id}`);
+				const res = await axios.delete(`https://book-roulette.herokuapp.com/books/${book._id}`);
 				if (res.status === 200) {
 					navigate('/');
 				}
@@ -85,47 +84,47 @@ function Book() {
                     <form onSubmit={handleSubmit}>
                         <div className="edit-container">
                             <div className="edit-label">
-                                <label htmlFor="edit-title">Title</label>
+                                <label htmlFor="title">Title</label>
                             </div>
                             <div className="edit-input">
-                                <input onChange={handleChange} id="edit-title" value={book.title} />
+                                <input onChange={handleChange} id="title" value={book.title} />
                             </div>
                         </div>
                         <div className="edit-container">
                             <div className="edit-label">
-                                <label htmlFor="edit-author">Author</label>
+                                <label htmlFor="author">Author</label>
                             </div>
                             <div className="edit-input">
-                                <input onChange={handleChange} id="edit-author" value={book.author} />
+                                <input onChange={handleChange} id="author" value={book.author} />
                             </div>
                         </div>
                         <div className="edit-container">
                             <div className="edit-label">
-                                <label htmlFor="edit-img">Book Cover URL</label>
+                                <label htmlFor="img">Book Cover URL</label>
                             </div>
                             <div className="edit-input">
-                                <input onChange={handleChange} id="edit-img" value={book.img} />
+                                <input onChange={handleChange} id="img" value={book.img} />
                             </div>
                         </div>
                         <div className="edit-container">
                             <div className="edit-label">
-                                <label htmlFor="edit-genre">Genre</label>
+                                <label htmlFor="genre">Genre</label>
                             </div>
                             <div className="edit-input">
-                                <input onChange={handleChange} id="edit-genre" value={book.genre} />
+                                <input onChange={handleChange} id="genre" value={book.genre} />
                             </div>
                         </div>
                         <div className="edit-container">
                             <div className="edit-label">
-                                <label htmlFor="edit-description">Synopsis</label>
+                                <label htmlFor="description">Synopsis</label>
                             </div>
                             <div className="edit-input">
-                                <input onChange={handleChange} id="edit-description" value={book.description} />
+                                <input onChange={handleChange} id="description" value={book.description} />
                             </div>
                         </div>
                         <div className="edit-container">
                             <div className="edit-label">
-                                <label htmlFor="edit-filmAdaptation">Adapted To Film?</label>
+                                <label htmlFor="filmAdaptation">Adapted To Film?</label>
                             </div>
                             <div className="edit-input">
                                 <input 
@@ -136,7 +135,7 @@ function Book() {
                                         filmAdaptation: !book.filmAdaptation,
                                     });
                                 }} 
-                                id="edit-filmAdaptation" 
+                                id="filmAdaptation" 
                                 value={book.filmAdaptation} 
                                 checked={book.filmAdaptation}
                             />
@@ -183,7 +182,7 @@ function Book() {
                                         </p>
                                         <p className="filmDetail">
                                             <span>Adapted to Film: </span>
-                                            {book.filmAdaptatioin ? "Yes" : "No"}
+                                            {book.filmAdaptation ? "Yes" : "No"}
                                         </p>
                                     </div>
                                 ) : (null) }
